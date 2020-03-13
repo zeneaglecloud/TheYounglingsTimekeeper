c2db00fe8a9a711c792a0b2a02904f82e14dbbf2
unknown <sizwe.hugo@gmail.com>
2020-03-13 11:04:12 +0200

    Uploaded Clock Action and Reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { firebaseReducer } from 'react-redux-firebase';
 
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
+import clockReducer from './clockReducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
+    clock: clockReducer,
     firebase: firebaseReducer
 });
 
