3d041e62acd75c73d6543d3e0dfd54d2068ca2eb
unknown <sizwe.hugo@gmail.com>
2020-03-23 10:12:12 +0200

    Added Link To Clock

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -8,6 +8,7 @@ const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/create'>Create Timesheet</NavLink></li>
+            <li><NavLink to='/timesheet'>Timesheet</NavLink></li>
             <li><a onClick={props.signOut}>Logout</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>JS</NavLink></li>
         </ul>
